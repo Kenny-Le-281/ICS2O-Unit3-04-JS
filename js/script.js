d7028ec707766e709db033198d3efa40a25b537f
Kenny Le <99355017+Kenny-Le-281@users.noreply.github.com>
2022-04-12 21:05:18 +0000

    fixed spelling

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@
 // This file contains the JS functions for index.html
 
 /**
- * Check servie worker.
+ * Check service worker.
  */
 if (navigator.serviceWorker) {
   navigator.serviceWorker.register("/ICS2O-Unit3-04-JS/sw.js", {
